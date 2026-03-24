6fac0ed64ffd7a2b0867fe799623cada3147dcc8
yegqr <yisakov@kse.org.ua>
2026-03-24 17:09:59 +0200

    deploy: 2026-03-24 17:09

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const sqlite3 = require('sqlite3').verbose();
 const fs = require('fs');
 const path = require('path');
 
-const DATA_DIR = path.join(__dirname, 'data');
+const DATA_DIR = process.env.DATA_DIR || path.join(__dirname, 'data');
 if (!fs.existsSync(DATA_DIR)) {
   fs.mkdirSync(DATA_DIR, { recursive: true });
 }
